0f173726b1feb58d217927d5f85317173df50e60
dmitry mineev <duke16@mail.ru>
2018-02-16 22:43:14 +0300

    open/close events

--- js/pictures.js
+++ js/pictures.js
@@ -9,7 +9,41 @@ var COMMENTS = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 var PHOTO_OBJECTS_NUMBER = 25;
+var ESC_KEYCODE = 27;
+var showGalleryOverlay = document.querySelector('.gallery-overlay');
+var uploadOverlay = document.querySelector('.upload-overlay');
+var uploadFile = document.querySelector('#upload-file');
+var uploadFormCancel = document.querySelector('.upload-form-cancel');
+var uploadEffectLevelPin = document.querySelector('.upload-effect-level-pin');
+/* var uploadEffectLevelValue = document.querySelector('.upload-effect-level-value');*/
 
+var onEditorFormEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeEditor();
+  }
+};
+
+var showEditor = function () {
+  uploadOverlay.classList.remove('hidden');
+  document.addEventListener('keydown', onEditorFormEscPress);
+};
+
+var closeEditor = function () {
+  uploadOverlay.classList.add('hidden');
+  document.addEventListener('keydown', onEditorFormEscPress);
+};
+
+uploadFile.addEventListener('change', function () {
+  showEditor();
+});
+
+uploadFormCancel.addEventListener('click', function () {
+  closeEditor();
+});
+
+uploadEffectLevelPin.addEventListener('mouseup', function () {
+  // расчитать положение пина слайдера
+});
 
 var getRandomRange = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -53,8 +87,6 @@ var renderPhotos = function () {
 
 var fillGallery = function (photosArray) {
   renderPhotos(photosArray);
-  var showGalleryOverlay = document.querySelector('.gallery-overlay');
-  showGalleryOverlay.classList.remove('hidden');
   showGalleryOverlay.querySelector('.gallery-overlay-image').src = photosArray[0].url;
   showGalleryOverlay.querySelector('.likes-count').textContent = photosArray[0].likes;
   showGalleryOverlay.querySelector('.comments-count').textContent = photosArray[0].comments.length;
